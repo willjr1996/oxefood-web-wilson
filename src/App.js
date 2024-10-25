b53e425cc63efdc51e3de5da1a9d267fc5443c70
aluno <aluno@ifpe>
2024-10-24 21:26:23 -0300

    atualização arquivos front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { Segment } from 'semantic-ui-react';
 import './App.css';
+import Home from "./views/home/Home"
 
 function App() {
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Home />
+
+      <div style={{ marginTop: '6%' }}>
+        <Segment vertical color='grey' size='tiny' textAlign='center'>
+          &copy; 2024 - Projeto WEB III - IFPE Jaboatão dos Guararapes
+        </Segment>
+      </div>
+
     </div>
   );
 }
---
+++ src/views/home/Home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Container, Grid, Image } from 'semantic-ui-react';
+
+export default function Home () {
+
+   return(
+       <div>
+
+           <div style={{marginTop: '5%'}}>
+               <Container>
+                   <Grid columns={2} divided>
+                       <Grid.Row>
+                           <Grid.Column>
+                               <Image src='/logo-IFPE.png' size='large' />
+                           </Grid.Column>
+                           <Grid.Column>
+                              
+                               Bem vindo ao sistema <strong>OxeFood</strong> ! <br/>
+                               Este sistema foi desenvolvido na disciplina de Desenvolvimento para WEB III. <br/> <br/>
+                               Para acessar o código da <strong>API</strong> do sistema, acesse: <a href='https://github.com/willjr1996/oxefood-api-wilson' target='_blank'> https://github.com/willjr1996/oxefood-api-wilson </a> <br/> <br/>
+                               Para acessar o código do <strong>Módulo WEB</strong>, acesse: <a href='https://github.com/willjr1996/oxefood-web-wilson' target='_blank'> https://github.com/willjr1996/oxefood-web-wilson </a>
+
+                           </Grid.Column>
+                       </Grid.Row>
+                   </Grid>
+               </Container>
+           </div>
+       </div>
+   )
+}
